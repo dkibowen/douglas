491fdbc70dc00e4d1e42a26bdb537f5d70e252ac
Douglas Kibowen <KibowenD@MRM_ARV.MRM.DOM>
2025-11-26 15:34:53 +0300

    added blogs app and created Category and Blog models

---
+++ blogs/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category,Blog
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("title",)}
+    list_display = ("id","title","category__category_name","author","status","is_featured","updated_at")
+    search_fields = ("id","title","status","author")
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog,BlogAdmin)
