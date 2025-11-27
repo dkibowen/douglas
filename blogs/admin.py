1c72b52cba43660e894cc5e742330cfb78adf75e
Douglas Kibowen <KibowenD@MRM_ARV.MRM.DOM>
2025-11-27 15:35:07 +0300

    added to homepage categories and blogs stored in models

--- blogs/admin.py
+++ blogs/admin.py
@@ -5,6 +5,7 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug":("title",)}
     list_display = ("title","category__category_name","author","status","is_featured","updated_at")
     search_fields = ("id","title","status","author")
+    list_editable = ("status","is_featured")
 
 # Register your models here.
 admin.site.register(Category)
