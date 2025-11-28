4066d3e9b0d40775d6ec85bd03231a82306cd7f6
Douglas Kibowen <KibowenD@MRM_ARV.MRM.DOM>
2025-11-28 14:17:46 +0300

    added posts view by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Blog, Category
 
 # Create your views here.
+def posts_by_category(request,pk):
+    posts = Blog.objects.filter(status='Published',category=pk)
+    try:
+        category = Category.objects.get(pk=pk)
+    except: 
+        return redirect('home')
+
+    context = {
+        "posts":posts,
+        "category":category,
+    }
+    return render(request,'posts_by_category.html',context)
