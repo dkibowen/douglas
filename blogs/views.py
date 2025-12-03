7288360afeac3b0cf117852133efa18f663748a3
Douglas Kibowen <KibowenD@MRM_ARV.MRM.DOM>
2025-12-03 08:41:52 +0300

    added search feature

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request,pk):
@@ -21,3 +22,12 @@ def single_blog(request,slug):
         "post":post,
     }
     return render(request,'blogs.html',context)
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword),status="Published")
+    context = {
+        "blogs":blogs,
+        "keyword":keyword,
+    }
+    return render(request,'search.html',context)
