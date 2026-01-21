702b83f654903749e2e22f695bcc7b2839f7a6a8
Douglas Kibowen <KibowenD@MRM_ARV.MRM.DOM>
2026-01-21 16:54:10 +0300

    added a post and modified footer

--- blogs/context_processors.py
+++ blogs/context_processors.py
@@ -1,5 +1,5 @@
 from .models import Category
 
 def get_categories(request):
-    categories = Category.objects.all().order_by('-created_at')
+    categories = Category.objects.all().order_by('updated_at')
     return dict(categories=categories)
